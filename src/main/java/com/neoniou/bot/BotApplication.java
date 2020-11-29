35072fec39323012c95b2b12de283abbf16ee511
aowubulao <635416616@qq.com>
2020-11-29 13:07:58 +0800

    Initial commit

---
+++ src/main/java/com/neoniou/bot/BotApplication.java
@@ -0,0 +1,16 @@
+package com.neoniou.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Neo.Zzj
+ * @date 2020/11/29
+ */
+@SpringBootApplication
+public class BotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotApplication.class, args);
+    }
+}
---
+++ src/main/java/com/neoniou/bot/core/MiraiBot.java
@@ -0,0 +1,8 @@
+package com.neoniou.bot.core;
+
+/**
+ * @author Neo.Zzj
+ * @date 2020/11/29
+ */
+public class MiraiBot {
+}
